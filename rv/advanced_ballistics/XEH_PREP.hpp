9bdfd0f42902b5aad1086bc217cccd322a875cb2
Dedmen Miller <dedmen@dedmen.de>
2018-08-10 18:33:27 +0200

    stuff

--- rv/advanced_ballistics/XEH_PREP.hpp
+++ rv/advanced_ballistics/XEH_PREP.hpp
@@ -6,9 +6,16 @@ PREP(calculateRetardation);
 PREP(calculateStabilityFactor);
 PREP(diagnoseWeapons);
 PREP(displayProtractor);
-PREP(handleFired);
+
+if (isNull (uiNamespace getVariable ["", scriptNull])) then {
+    PREP(handleFired);
+    PREP(handleFirePFH);
+} else {
+	
+};
+
+
 PREP(initializeTerrainExtension);
 PREP(initModuleSettings);
 PREP(readAmmoDataFromConfig);
 PREP(readWeaponDataFromConfig);
-PREP(handleFirePFH);
